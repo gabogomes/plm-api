869343c06ee7e81d437ad4ddce4896c56bf61441
Gabriel Gomes <aulasconsultorias@gmail.com>
2024-02-03 00:05:53 -0300

    feature: including e-mail client settings.

--- plm/settings.py
+++ plm/settings.py
@@ -5,6 +5,8 @@ from pydantic import BaseSettings
 class PlmSettings(BaseSettings):
     db_name: str
     db_username: str
+    plm_email_address: str
+    plm_email_password: str
     db_password: Optional[str]
     local_db_host: Optional[str]
     local_db_port: Optional[int]
